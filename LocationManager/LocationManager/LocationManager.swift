ef4b8310c73d0aadea78f40866a45437211ff53e
Sean Kladek <sean@303software.com>
2017-05-12 12:19:33 -0600

    Added methods to check availability

--- LocationManager/LocationManager/LocationManager.swift
+++ LocationManager/LocationManager/LocationManager.swift
@@ -10,18 +10,31 @@ import CoreLocation
 import UIKit
 
 class LocationManager: NSObject {
+    enum ErrorCode: Int {
+        case unknown = -1
+        case locationServicesDisabled
+        case authorizationStatusDenied
+        case authorizationStatusNotDetermined
+        case authorizationStatusRestricted
+    }
+
     enum PermissionType {
         case always
         case whenInUse
     }
 
-    typealias LocationRequest = (_ location: CLLocation?, _ error: Error?) -> ()
+    typealias Availability = (available: Bool, error: NSError?)
+
+    typealias LocationRequest = (_ location: CLLocation?, _ error: NSError?) -> ()
+
+    static let errorDomain = "com.skladek.locationManager"
 
     let locationManager = CLLocationManager()
 
     var oneTimeLocationRequestCompletion: LocationRequest?
 
     let permissionType: PermissionType
+
 
     init(permissionType: PermissionType) {
         self.permissionType = permissionType
@@ -39,16 +52,78 @@ class LocationManager: NSObject {
         }
     }
 
+    func requestAvailability() -> Availability {
+        if let locationServicesUnavailability = locationSerivicesEnabled() {
+            return locationServicesUnavailability
+        }
+
+        if let authorizationStatusUnavailability = authorizationStatus() {
+            return authorizationStatusUnavailability
+        }
+
+        return (available: true, error: nil)
+    }
+
     func requestLocation(_ completion: @escaping LocationRequest) {
         oneTimeLocationRequestCompletion = completion
         locationManager.requestLocation()
+    }
+
+    private func authorizationStatus() -> Availability? {
+        let code: ErrorCode
+        let message: String
+
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways,
+             .authorizedWhenInUse:
+            return (available: true, error: nil)
+        case .denied:
+            code = .authorizationStatusDenied
+            message = "Location services permission denied."
+        case .notDetermined:
+            code = .authorizationStatusNotDetermined
+            message = "Location services authorization is not yet determined."
+        case .restricted:
+            code = .authorizationStatusRestricted
+            message = "This app is not authorized to use location services."
+        }
+
+        let error = self.error(code: code, message: message)
+
+        return (available: false, error: error)
+    }
+
+    fileprivate func error(code: ErrorCode, message: String?) -> NSError {
+        var localizedDescription = message
+
+        if code == .unknown && message == nil {
+            localizedDescription = "An unknown error occurred."
+        }
+
+        var userInfo: [AnyHashable : Any]? = nil
+        if let message = localizedDescription {
+            userInfo = [NSLocalizedDescriptionKey : message]
+        }
+
+        return NSError(domain: LocationManager.errorDomain, code: code.rawValue, userInfo: userInfo)
+    }
+
+    private func locationSerivicesEnabled() -> Availability? {
+        if !CLLocationManager.locationServicesEnabled() {
+            let error = self.error(code: .locationServicesDisabled, message: "Location services is currently disabled")
+
+            return (available: false, error: error)
+        }
+
+        return nil
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         if let oneTimeRequest = oneTimeLocationRequestCompletion {
-            oneTimeRequest(nil, error)
+            let locationError = self.error(code: .unknown, message: error.localizedDescription)
+            oneTimeRequest(nil, locationError)
         }
     }
 
--- LocationManager/ViewControllers/HomeViewController.swift
+++ LocationManager/ViewControllers/HomeViewController.swift
@@ -9,22 +9,43 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    var locationManager: LocationManager?
+    let locationManager: LocationManager
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        locationManager = LocationManager(permissionType: .whenInUse)
+
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @IBAction func requestAvailiabilityTapped() {
+        let availability = locationManager.requestAvailability()
+
+        if let error = availability.error {
+            print(error.localizedDescription)
+            if error.code == LocationManager.ErrorCode.authorizationStatusNotDetermined.rawValue {
+                // Request permission
+            } else {
+                // Handle denied/restricted/disabled state
+            }
+
+            return
+        }
+
+        // Location services is available
+    }
 
     @IBAction func requestLocationOnce() {
-        locationManager?.requestLocation({ (location, error) in
+        locationManager.requestLocation({ (location, error) in
             print("Location: \(String(describing: location))")
             print("Error: \(String(describing: error))")
         })
     }
 
     @IBAction func requestPermissionTapped() {
-        locationManager?.requestAuthorization()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        locationManager = LocationManager(permissionType: .whenInUse)
+        locationManager.requestAuthorization()
     }
 }
