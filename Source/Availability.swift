94686cba6ff45adb10098cffa0a20b809263b38b
Sean Kladek <sean@303software.com>
2017-07-12 15:34:25 -0600

    Changed the availability error to be more specific

--- Source/Availability.swift
+++ Source/Availability.swift
@@ -11,7 +11,7 @@ import Foundation
 
 public protocol Availability {
     // A tuple to describe availability status with an error if the service is unavailable.
-    typealias AvailabilityStatus = (available: Bool, error: NSError?)
+    typealias AvailabilityStatus = (available: Bool, error: LocationError?)
 
     func authorizationStatus(_ authorizationStatus: CLAuthorizationStatus) -> AvailabilityStatus
     func servicesEnabled(_ enabled: Bool) -> AvailabilityStatus
